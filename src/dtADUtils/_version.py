c092abbfffca7c262e5639d563ab630c4b5a341b
Matthew <matthew@digital-thought.org>
2022-12-22 15:18:02 +1100

    Initial commit

---
+++ src/dtADUtils/_version.py
@@ -0,0 +1,8 @@
+_VERSION = '1.0.0'
+_MAINTAINER = 'Matthew @ Digital-Thought'
+_AUTHOR = 'Digital-Thought'
+_AUTHOR_EMAIL = 'matthew@digital-thought.org'
+_DESCRIPTION = "Python utilities to work with Active Directory"
+_TITLE = "dtADUtils"
+_PROJECT_URL = ""
+_DOCS_URL = ""
